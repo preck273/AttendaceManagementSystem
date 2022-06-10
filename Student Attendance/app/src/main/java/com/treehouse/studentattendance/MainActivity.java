9990c9c0aa36c32032b1905486ddab819dddd398
samysosa1 <73364324+samysosa1@users.noreply.github.com>
2022-06-10 16:43:09 +0200

    Action bar
    Modify Action bar to show logo

--- Student Attendance/app/src/main/java/com/treehouse/studentattendance/MainActivity.java
+++ Student Attendance/app/src/main/java/com/treehouse/studentattendance/MainActivity.java
@@ -1,11 +1,15 @@
 package com.treehouse.studentattendance;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -17,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //action bar
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_background));
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //custom image for action bar
+        customActionBar(actionbar);
+
 
         //when the app starts, the Report page fragment shows first
         replaceFragment(new ReportFragment());
@@ -42,6 +55,14 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
 
+    }
+
+    //custom image for action bar 
+    private void customActionBar(ActionBar actionbar) {
+        actionbar.setDisplayShowCustomEnabled(true);
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.custom_header, null);
+        actionbar.setCustomView(view);
     }
 
     //method to replace the frame layout in the main activity layout by the fragment layouts
