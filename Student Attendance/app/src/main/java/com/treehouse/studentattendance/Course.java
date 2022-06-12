61248c9a713890e3f2c5f20ea930517cc6500f33
samysosa1 <73364324+samysosa1@users.noreply.github.com>
2022-06-13 01:06:48 +0200

    .

---
+++ Student Attendance/app/src/main/java/com/treehouse/studentattendance/Course.java
@@ -0,0 +1,35 @@
+package com.treehouse.studentattendance;
+
+public class Course {
+    private String courseName, startDate, endDate;
+
+    public Course(String courseName, String startDate, String endDate) {
+        this.courseName = courseName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
---
+++ Student Attendance/app/src/main/java/com/treehouse/studentattendance/CourseAdapter.java
@@ -0,0 +1,47 @@
+package com.treehouse.studentattendance;
+
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * RecyclerView.Adapter - binds the data to the view
+ * RecyclerView.ViewHolder -  holds the view
+ */
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseViewHolder> {
+//we place the viewHolder inside the <> above, since it is inside Adapter class, you first
+// have to mention the parent class : CourseAdapter, then the subclass: CourseViewHolder
+
+    private Context context;
+    private List<Course> courseList;
+
+    @NonNull
+    @Override
+    public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    //viewHolder class created inside the adapter class
+    class CourseViewHolder extends RecyclerView.ViewHolder{
+
+        public CourseViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
