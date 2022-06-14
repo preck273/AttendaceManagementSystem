fd6a4caf7cfd79dcbb8fddbe75157a17c433bd6a
samysosa1 <73364324+samysosa1@users.noreply.github.com>
2022-06-14 11:05:35 +0200

    couirse class
    Work on the course page, should display student chosen courses, currently not showing anything, got to fix that bug

--- StudentAttendance/app/src/main/java/com/treehouse/studentattendance/CourseAdapter.java
+++ StudentAttendance/app/src/main/java/com/treehouse/studentattendance/CourseAdapter.java
@@ -2,8 +2,11 @@ package com.treehouse.studentattendance;
 
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,27 +24,49 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
     private Context context; // needed to inflate the course layout
     private List<Course> courseList;
 
+    //constructor
+    public CourseAdapter(Context context, List<Course> courseList) {
+        this.context = context;
+        this.courseList = courseList;
+    }
+
     @NonNull
     @Override
     public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.course_layout, null);
+        return new CourseViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
+        // the holder instance variable (of the CourseViewHolder class below
+        // holds the UI element and with the position var,
+        //we can get the specified item from the list.
+        Course course = courseList.get(position);
+
+        holder.textViewTitle.setText(course.getCourseName());
+        holder.textViewStartDate.setText(course.getStartDate());
+        holder.textViewEndDate.setText(course.getEndDate());
 
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
+    public int getItemCount() { // return the size of the list : courseList
+        return courseList.size(); // if you return 0, the recycler view will not display any items.
     }
 
     //viewHolder class created inside the adapter class
     class CourseViewHolder extends RecyclerView.ViewHolder{
 
+        TextView textViewTitle, textViewStartDate, textViewEndDate;
+
         public CourseViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.course_layout_title_TV);
+            textViewStartDate = itemView.findViewById(R.id.course_layout_startDate_TV);
+            textViewEndDate = itemView.findViewById(R.id.course_layout_endDate_TV);
         }
     }
 }
--- StudentAttendance/app/src/main/java/com/treehouse/studentattendance/CourseFragment.java
+++ StudentAttendance/app/src/main/java/com/treehouse/studentattendance/CourseFragment.java
@@ -8,6 +8,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -15,21 +16,41 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CourseFragment extends Fragment {
 
     View view;
+
     RecyclerView recyclerView;
+    CourseAdapter adapter;
+
+    List<Course> listOfCourses;
+
     Button attendBtn;
     ImageView courseList;
+
+    private static final String COURSE_URL = "http://192.168.1.105/attendanceApp/get_attendance_detail.php";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view =  inflater.inflate(R.layout.fragment_course, container, false);
-
+        view = inflater.inflate(R.layout.fragment_course, container, false);
 
 
         return view;
@@ -40,9 +61,17 @@ public class CourseFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         //initialize view here
-        recyclerView = view.findViewById(R.id.courseRecyclerView);
         attendBtn = view.findViewById(R.id.course_attend_btn);
         courseList = view.findViewById(R.id.course_courseList_IV);
+
+        listOfCourses = new ArrayList<>();
+
+        recyclerView = view.findViewById(R.id.courseRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity())); // this set the layout vertical
+
+        loadCourses();
+
 
         //attend button listener
         attendBtn.setOnClickListener(new View.OnClickListener() {
@@ -64,7 +93,52 @@ public class CourseFragment extends Fragment {
         });
     }
 
-    private void switchFragment(Fragment fragment){
+    //get courses from URL
+    private void loadCourses() {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, COURSE_URL,
+                new Response.Listener<String>() {  //this method will be executed when there is no error
+                    @Override
+                    public void onResponse(String response) {
+                        //array of data that we'll save from the URL
+                        //first convert to Json array
+                        try {
+                            JSONArray courses = new JSONArray(response);
+                            //now loop through all items in the array
+                            for (int i = 0; i < courses.length(); i++) {
+                                JSONObject courseObject = courses.getJSONObject(i);
+
+                                String name = courseObject.getString("courseName");
+                                String startDate = courseObject.getString("startTime");
+                                String endDate = courseObject.getString("endTime");
+
+                                //now create course object
+                                Course course = new Course(name, startDate, endDate);
+
+                                //add this course to the course list
+                                listOfCourses.add(course);
+                            }
+
+                            adapter = new CourseAdapter(getContext(), listOfCourses);
+                            recyclerView.setAdapter(adapter);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) { //when we have errors
+                        Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+        Volley.newRequestQueue(getContext()).add(stringRequest);
+    }
+
+    private void switchFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
